fb02c3aedd454a9653a49bc30e00bf575ae7ab6c
rv178 <idliyout@gmail.com>
2023-06-08 22:55:22 +0530

    [rm] ccls cache

--- .ccls-cache/@home@rv@extras@cpp@nfsmw-rpc/src@config.h
+++
@@ -1,6 +0,0 @@
-#define APP_ID "1116406283499741286" //* - SC: these don't have to be changed as long as wictornogueira doesn't delete the app
-#define IMG_KEY "logo" //todo - SC: change to our own app, but in case this gets forked, you can make your own app at https://discord.com/developers/applications
-#define IMG_TXT "Need for Speed: Most Wanted"
-#define UPD_INTVL 5000
-
-#define IN_MENU "Main Menu" // ANY menu
